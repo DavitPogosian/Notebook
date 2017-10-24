d797775907ba23b5e39a330e5f57f404089c432d
IradaMaheramova <mageramovai@gmail.com>
2017-10-24 12:38:05 +0200

    ya zdelala slova

--- app/src/main/java/com/example/davit/notebook/WordsActivity.java
+++ app/src/main/java/com/example/davit/notebook/WordsActivity.java
@@ -7,6 +7,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
@@ -23,31 +24,42 @@ public class WordsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.words);
-        t= (TextView) findViewById(R.id.txt);
-        Word= (ListView)  findViewById(R.id.word);
+        t = (TextView) findViewById(R.id.txt);
+        Word = (ListView) findViewById(R.id.word);
         mydb = new DBhelper(context);
         //String s=getIntent().getExtras().getString("Letter");
-        String letter=getIntent().getExtras().getString("Letter");
-        Log.e("tis is sparta","l= "+letter);
+        String letter = getIntent().getExtras().getString("Letter");
+        Log.e("tis is sparta", "l= " + letter);
 
-       final Cursor res = mydb.GetWordByLetterInEn(letter);
+        final Cursor res = mydb.GetWordByLetterInEn(letter);
         //final Cursor res = mydb.GetAll();
 
 //        t.setText(letter);
         //Log.e("this","a"+getIntent().getExtras().getInt("Button"));
 
-        if(getIntent().getExtras().getInt("Button") > 0) {
+        if (getIntent().getExtras().getInt("Button") > 0) {
             b = (Button) findViewById(getIntent().getExtras().getInt("Button"));
             b.setBackgroundColor(getResources().getColor(R.color.colorAccent));
-        }else
-        {
+        } else {
             b = (Button) findViewById(R.id.bA);
             b.setBackgroundColor(getResources().getColor(R.color.colorAccent));
 
         }
-        Word.setAdapter(new Adapter (res,this));
+        Word.setAdapter(new Adapter(res, this));
+        this.Word.setOnItemClickListener(new AdapterView.OnItemClickListener() {
 
 
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                res.move(position);
+                Log.d("ProductID", res.getString(0));
+                Intent go = new Intent(getApplicationContext(), EditActivity.class);
+                go.putExtra("ID", res.getInt(0));
+                res.moveToFirst();
+                startActivity(go);
+            }
+        });
     }
 
 
